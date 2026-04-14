b4e6f63ab28a3d0b920ca071903bc569c5a37721
Retiez <159192800+Manuzel@users.noreply.github.com>
2026-04-14 10:16:53 -0300

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ youtubestream = youtube.streams.get_highest_resolution()
 titulovideo = youtube.title
 print (titulovideo)
 
-videopath = youtubestream.download(output_path = "/home/manuzel/Documentos/Programas/Python/videos")
+videopath = youtubestream.download(output_path = "YOUR_PATH_VIDEO")
 
 audio = VideoFileClip(videopath).audio
-audio.write_audiofile(f"/home/manuzel/Documentos/Programas/Python/audios/{titulovideo}.mp3")
+audio.write_audiofile(f"YOUR_PATH_AUDIO{titulovideo}.mp3")
